bd3506ea5f0fc92bf5d4802d6e2c0ef1899455e4
Al Schaapman <me@alschaapman.com>
2019-10-24 14:35:54 -0700

    Give App component its own file

---
+++ client/App.jsx
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import React from 'react';
+import UserReview from './UserReview.jsx';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      reviews: [],
+    };
+  }
+
+  componentDidMount() {
+    axios({
+      method: 'GET',
+      url: 'http://localhost:65387/api/user-reviews',
+    }).then((response) => {
+      this.setState({ reviews: response.data });
+    });
+  }
+
+  render() {
+    const { reviews } = this.state;
+    return (
+      <div className="user-reviews">
+        <h2>User Reviews</h2>
+        { reviews.length && <UserReview reviews={reviews} />}
+      </div>
+    );
+  }
+}
+
+export default App;
