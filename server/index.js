1bc0fbf9e01b3bab1bb88c844de0ae80afad9527
Al Schaapman <me@alschaapman.com>
2019-10-24 12:02:36 -0700

    Use cors

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const db = require('../database');
 
 const app = express();
 const PORT = 65387;
 
 app.use(express.static(path.join(__dirname, '/../public')));
+app.use(cors());
 
 app.get('/api/user-reviews', (req, res) => {
   db.getAllReviews((err, data) => {
