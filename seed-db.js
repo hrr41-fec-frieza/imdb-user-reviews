1b87aa134b3f8a0298001e601d440e5387c4c93c
Al Schaapman <me@alschaapman.com>
2019-10-25 12:56:28 -0700

    Create db, movies table

--- seed-db.js
+++ seed-db.js
@@ -1,8 +1,46 @@
+const Promise = require('bluebird');
 const casual = require('casual');
-const db = require('./database');
+const mysql = require('mysql');
+const { user, password, database } = require('./database/config.js');
+
+const createDatabase = () => {
+  const db = mysql.createConnection({ user, password });
+  const sql = 'CREATE DATABASE IF NOT EXISTS mooviesdb_user_reviews;';
+  const callback = () => { db.end(); };
+
+  db.query(sql, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      callback(null, data);
+    }
+  });
+};
 
 const seedDatabase = (callback = () => {}) => {
-  let sql = 'DROP TABLE IF EXISTS reviews;';
+  const db = mysql.createConnection({ user, password, database });
+  let sql = '';
+
+  db.query('DROP TABLE IF EXISTS reviews;', (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      callback(null, data);
+    }
+  });
+
+  db.query('DROP TABLE IF EXISTS movies;', (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      callback(null, data);
+    }
+  });
+
+  sql = `CREATE TABLE movies (
+    id INT NOT NULL,
+    title VARCHAR(175)
+    );`;
 
   db.query(sql, (err, data) => {
     if (err) {
@@ -23,7 +61,7 @@ const seedDatabase = (callback = () => {}) => {
     recs INT NOT NULL,
     rectotal INT NOT NULL,
     PRIMARY KEY (id)
-  );`;
+    );`;
 
   db.query(sql, (err, data) => {
     if (err) {
@@ -35,11 +73,11 @@ const seedDatabase = (callback = () => {}) => {
 
   for (let i = 0; i < 100; i += 1) {
     const username = casual.username;
-    const date = casual.date('MMDD');
+    const date = casual.date('YYYYMMDD');
     const rating = casual.integer(0, 10);
     const headline = casual.sentence;
     const body = casual.sentences(casual.integer(1, 15));
-    const record = `"${username}", "2019${date}", ${rating}, "${headline}", "${body}", b'0', 50, 100`;
+    const record = `"${username}", "${date}", ${rating}, "${headline}", "${body}", b'0', 50, 100`;
     sql = `INSERT INTO reviews (username, date, rating, headline, body, spoilers, recs, rectotal) VALUES(${record})`;
     db.query(sql, (err, data) => {
       if (err) {
@@ -51,4 +89,8 @@ const seedDatabase = (callback = () => {}) => {
   }
 };
 
-seedDatabase();
+Promise.try(() => {
+  createDatabase();
+}).then(() => {
+  seedDatabase();
+});
